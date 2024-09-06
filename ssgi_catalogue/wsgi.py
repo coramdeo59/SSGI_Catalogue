a2a8abbcf9ff372c59d52ec278eb627528481dba
melkamu Elias <134687821+coramdeo59@users.noreply.github.com>
2024-09-06 10:25:58 -0400

    wsig add

--- ssgi_catalogue/wsgi.py
+++ ssgi_catalogue/wsgi.py
@@ -1,6 +1,8 @@
-# 
+import sys
 import os
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usgsfeatures.settings')
 
