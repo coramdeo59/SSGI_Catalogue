967a2cf12b5010a97e3e073110c926b8382c0dc5
melkamu Elias <134687821+coramdeo59@users.noreply.github.com>
2024-09-06 15:34:09 -0400

    settings

--- usgsfeatures/settings.py
+++ usgsfeatures/settings.py
@@ -8,11 +8,8 @@ load_dotenv()
 
 # Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'ssgi-catalogue-vtej.onrender.com',
-]
+
+ALLOWED_HOSTS = ['ssgi-catalogue-vtej.onrender.com']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
