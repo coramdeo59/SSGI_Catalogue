34dc45a9dae98dff4687404e32c156a44737c7da
melkamu Elias <134687821+coramdeo59@users.noreply.github.com>
2024-09-06 19:18:25 -0400

    ADD

--- usgsfeatures/settings.py
+++ usgsfeatures/settings.py
@@ -158,11 +158,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 FILE_UPLOAD_PERMISSIONS = 0o640
 
 
-import os
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  # Default to 587 if not set
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
